e166c5d6f35afc40606d1a04bf682abe11bbf47d
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-23 13:04:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     a = Time.new
-    resp.write "#{a}"
+    resp.write "#{a}\n"
     resp.write "#{a.hour}"
     resp.finish
   end
